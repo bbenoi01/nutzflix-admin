aa0dba83b5d9ccd019b396847213c4472016c6c4
Brandon Benoit <b.nutty@gmail.com>
2022-03-31 21:00:04 -0700

    completed styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,51 @@
+import {
+	BrowserRouter as Router,
+	Routes as Switch,
+	Route,
+	Navigate,
+} from 'react-router-dom';
+import { productInputs, userInputs } from './formSource';
+import './style/dark.scss';
+
+import Home from './pages/home/Home';
+import Login from './pages/login/Login';
+import List from './pages/list/List';
+import Single from './pages/single/Single';
+import New from './pages/new/New';
+import { useContext } from 'react';
+import { DarkModeContext } from './context/darkModeContext';
+
 const App = () => {
-	return <div>App</div>;
+	const { darkMode } = useContext(DarkModeContext);
+
+	return (
+		<div className={darkMode ? 'app dark' : 'app'}>
+			<Router>
+				<Switch>
+					<Route path='/'>
+						<Route index element={<Home />} />
+						<Route path='login' element={<Login />} />
+						<Route path='users'>
+							<Route index element={<List />} />
+							<Route path=':userId' element={<Single />} />
+							<Route
+								path='new'
+								element={<New inputs={userInputs} title='Add New User' />}
+							/>
+						</Route>
+						<Route path='products'>
+							<Route index element={<List />} />
+							<Route path=':productId' element={<Single />} />
+							<Route
+								path='new'
+								element={<New inputs={productInputs} title='Add New Product' />}
+							/>
+						</Route>
+					</Route>
+				</Switch>
+			</Router>
+		</div>
+	);
 };
 
 export default App;
